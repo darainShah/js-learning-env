d560b9d7dc281765d1aff007233f6f5cfe8d7460
darainShah <162487006+darainShah@users.noreply.github.com>
2024-10-13 06:58:31 +0000

    changes success

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";  // Use all code as a newer version of js
+
+//alert(3 + 3) // this is node.js not browser so doesnot pop up 6 
+
+let name = "Darain"
+let age = 18
+let isloggedin = true
+let temp = null
+ // these are datatypes
+ // bigint 
+ // null -> standalone khali h
+ // undefined -> value is not empty but undefined
+
+ console.log(typeof(age));
+ console.log(typeof(null));
+
+
+
