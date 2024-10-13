d6442556a03f40e189fa4fa6ac1d27684da4782c
darainShah <162487006+darainShah@users.noreply.github.com>
2024-10-13 06:45:30 +0000

    changes done succesfully

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -1,5 +1,5 @@
 const AccountId = 55432
-let accointemail = "darainshahedi1729@gmail.com"
+let accountemail = "darainshahedi1729@gmail.com"
 var accountpassword = "12345"
 accountcity = "surat"
 
@@ -7,5 +7,20 @@ accountcity = "surat"
 //AccountId = 12345
 
 console.log(AccountId);
+
+accountemail = "shahedi1729@gmai.com"
+accountcity = "New Delhi"
+accountpassword = "12121"
+
+let accountState  // take undefined value 
+
+
+/*
+Donot use var because of functional scope and block scope 
+*/
+
+console.table([AccountId, accountemail, accountcity, accountpassword, accountState]);
+
+
 
 
