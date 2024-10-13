42d111e49e45c13f56722bf0c8145413e13b6fd6
darainShah <162487006+darainShah@users.noreply.github.com>
2024-10-13 06:36:59 +0000

    container commit success

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const AccountId = 55432
+let accointemail = "darainshahedi1729@gmail.com"
+var accountpassword = "12345"
+accountcity = "surat"
+
+// Not allowed as const cannot be changed
+//AccountId = 12345
+
+console.log(AccountId);
+
+
