f16241495b38855c103193734fc03288252cc671
darainShah <162487006+darainShah@users.noreply.github.com>
2024-10-13 07:13:23 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+console.log(score);
+
+let valueinNumber = Number(score)
+console.log(valueinNumber);
+console.log(score);
+
