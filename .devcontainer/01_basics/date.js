f47447255246f45731d21dd1aa598912ef3bf6b6
darainShah <162487006+darainShah@users.noreply.github.com>
2024-10-15 06:02:56 +0000

    success

--- .devcontainer/01_basics/date.js
+++ .devcontainer/01_basics/date.js
@@ -7,3 +7,11 @@ const mydate = new Date()
 let mycreatedDate = new Date(2023, 0, 27, 5, 4, 53)
 console.log(mycreatedDate.toLocaleString)
 
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(mycreatedDate.getTime())
+
+let newdate = new Date()
+console.log(newdate)
+console.log(newdate.getTimezoneOffset())
+console.log(newdate.getMonth())
